709d18e66d501355a431a49cda24121221e9a154
Rodrigo Luger <rodluger@gmail.com>
2016-09-20 11:42:27 -0300

    update figure2 script

--- scripts/figure2.py
+++ scripts/figure2.py
@@ -16,16 +16,13 @@ import numpy as np
 np.random.seed(123)
 
 # Build up FAP
-#search.Compute(plot = False, fap_iter = 5000)
+search.Compute(plot = False, fap_iter = 5000)
 
 # Injection detection plot (8 sigma)
-#search.Plot(frame = 'planet', inject_contrast = 8e-3, crop_outliers = True, fap_iter = 0,
-#             figname = 'strong.pdf', suptitle = 'Injected')
+search.Plot(frame = 'planet', inject_contrast = 8e-3, crop_outliers = True, fap_iter = 0,
+             figname = 'strong.pdf', suptitle = 'Injected')
 
-# Search
-search.Search(orb_iter = 100, inclination = [55.])
-quit()
-
+# The actual "detection" (~5 sigma)
 planet = search.ProxCenB()
 planet.inclination = 55
 planet.mass = 1.55
@@ -33,4 +30,4 @@ planet.period = 11.18349
 planet.stellar_mass = 0.11
 planet.mean_longitude = 117.26
 search.Plot(frame = 'planet', inject_contrast = 0, crop_outliers = True, fap_iter = 0,
-            figname = 'fourth_quarter.pdf', planet = planet, load_fap = False)
+            figname = 'real.pdf', planet = planet, load_fap = False)
