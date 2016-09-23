e3e5513124e4d1295faea827578f2956f849bc7b
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2016-09-23 16:47:06 -0700

    added contour lines

--- detectability/eq_width.py
+++ detectability/eq_width.py
@@ -14,6 +14,7 @@ import contrast_utils as cu
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.colors as colors
+import matplotlib.ticker as ticker
 
 #Typical plot parameters that make for pretty plots
 mpl.rcParams['figure.figsize'] = (10,8)
@@ -70,80 +71,6 @@ for ii in range(len(watts)):
                                                      wave_hires[mask],
                                                      continuum)
 
-if show_contour:
-
-    # Contrast, Resolving power, auroral power grid
-    bins = 100
-
-    watts = np.logspace(9,15,bins)
-    resolver = np.logspace(1,6,bins)
-
-    contrast = np.zeros((bins,bins))
-    ew = np.zeros_like(watts)
-
-    for ii in range(len(watts)):
-        for jj in range(len(resolver)):
-
-            # Compute auroral flux line profile and hi-res wavelength array
-            # Compute delta_lam
-            dl = lam_0/resolver[jj]
-            FWHM = 0.0186*1.0e-4
-            wave_hires = cu.make_wave_array(lam_0,dl,FWHM)
-
-            aurora = cu.create_auroral_line(lam_0, watts[ii], 1.0, proxcen,
-                                            wave_hires)
-
-            # Using star's spectrum, get stellar flux over the hi-res array
-            # Note: stellar spectrum is R~10,000, so flux will be interpolated
-            stellar_flux = proxcen.star.spectrum(wave_hires)
-
-            # Now compute the reflected flux at quadrature assuming a grey planet with
-            # geometric albedo = 0.3
-            ref_flux = stellar_flux * proxcen.contrast_ratio(90.0)
-
-            # Compute total planetary flux (reflected + auroral)
-            planet_flux = aurora + ref_flux
-
-            num = np.trapz(planet_flux,wave_hires)
-            denom = np.trapz(stellar_flux,wave_hires)
-            contrast[ii,jj] = num/denom
-            # End inner loop
-
-        # Compute equivalent width for each power step
-
-        # Compute continuum
-        continuum = np.median(ref_flux)
-        ew[ii] = cu.equivalent_width(planet_flux,wave_hires,continuum)
-
-    # Compute equivlent resolving power (?)
-    ew_r = lam_0/ew
-
-    fig, ax = plt.subplots()
-
-    vmin = contrast.min()
-    vmax = contrast.max()
-
-    cax = ax.pcolor(watts, resolver, contrast.T, cmap="viridis",
-                    norm=colors.LogNorm(vmin=vmin,vmax=vmax))
-
-    # Colorbar
-    cbar = fig.colorbar(cax)
-    cbar.set_label(r"Planet-Star Contrast", rotation=270, labelpad=25)
-
-    ax.plot(watts,ew_r, color="white", lw=3, ls="--")
-
-    # Format
-    ax.set_ylim(resolver.min(),resolver.max())
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-
-    # Axis lables
-    ax.set_xlabel(r"OI Auroral Power [W]")
-    ax.set_ylabel(r"Resolving Power")
-
-    if save_plots:
-        fig.savefig("contrast_vs_R_vs_watts.pdf")
-
 # Plot Planet flux
 if show_plots:
 
@@ -164,7 +91,7 @@ if show_plots:
     if save_plots:
         fig.savefig("eq_vs_power.pdf")
 
-    plt.clf()
+    #plt.clf()
 
     # Plot 1 test case
 
@@ -223,3 +150,88 @@ if show_plots:
 
     if save_plots:
         fig.savefig("ew_test_case.pdf")
+
+if show_contour:
+
+    # Contrast, Resolving power, auroral power grid
+    bins = 100
+
+    watts = np.logspace(9,15,bins)
+    resolver = np.logspace(1,6,bins)
+
+    contrast = np.zeros((bins,bins))
+    ew = np.zeros_like(watts)
+
+    for ii in range(len(watts)):
+        for jj in range(len(resolver)):
+
+            # Compute auroral flux line profile and hi-res wavelength array
+            # Compute delta_lam
+            dl = lam_0/resolver[jj]
+            FWHM = 0.0186*1.0e-4
+            wave_hires = cu.make_wave_array(lam_0,dl,FWHM)
+
+            aurora = cu.create_auroral_line(lam_0, watts[ii], 1.0, proxcen,
+                                            wave_hires)
+
+            # Using star's spectrum, get stellar flux over the hi-res array
+            # Note: stellar spectrum is R~10,000, so flux will be interpolated
+            stellar_flux = proxcen.star.spectrum(wave_hires)
+
+            # Now compute the reflected flux at quadrature assuming a grey planet with
+            # geometric albedo = 0.3
+            ref_flux = stellar_flux * proxcen.contrast_ratio(90.0)
+
+            # Compute total planetary flux (reflected + auroral)
+            planet_flux = aurora + ref_flux
+
+            # Integrate planet and star flux across spectral element;
+            # divide for contrast ratio
+            num = np.trapz(planet_flux,wave_hires)
+            denom = np.trapz(stellar_flux,wave_hires)
+            contrast[ii,jj] = num/denom
+            # End inner loop
+
+        # Compute equivalent width for each power step
+
+        # Compute continuum
+        continuum = np.median(ref_flux)
+        ew[ii] = cu.equivalent_width(planet_flux,wave_hires,continuum)
+
+    # Compute equivlent resolving power (?)
+    ew_r = lam_0/ew
+
+    fig, ax = plt.subplots()
+
+    vmin = contrast.min()
+    vmax = contrast.max()
+
+    # Color contour
+    cax = ax.pcolor(watts, resolver, contrast.T, cmap="viridis",
+                    norm=colors.LogNorm(vmin=vmin,vmax=vmax))
+
+    # Contour Lines
+    contour_levels = np.array([1e-6, 1e-5, 1e-4, 1e-3, 1e-2])
+    fmt = ticker.LogFormatterMathtext()
+    cln = ax.contour(watts, resolver, contrast.T, contour_levels, colors="black", lw=3.0)
+    plt.clabel(cln, inline=1, fontsize=14, fmt=fmt)
+
+    # Colorbar
+    cbar = fig.colorbar(cax)
+    cbar.set_label(r"Planet-Star Contrast", rotation=270, labelpad=25)
+
+    ax.plot(watts,ew_r, color="white", lw=3, ls="--")
+
+    # Format
+    ax.set_ylim(resolver.min(),resolver.max())
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    # Axis lables
+    ax.set_xlabel(r"OI Auroral Power [W]")
+    ax.set_ylabel(r"Resolving Power")
+
+    plt.show()
+
+    if save_plots:
+        fig.savefig("contrast_vs_R_vs_watts.pdf")
